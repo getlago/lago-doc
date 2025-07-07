fe9a9662238ca507a8a8b6d557f5955d2d67e6f9
Adam Albarghouthi <adambargh@gmail.com>
2025-07-07 15:41:30 -0400

    feat: add hs script

--- tracking.js
+++ tracking.js
@@ -1,1 +1,4 @@
-<script id="warmly-script-loader" src="https://opps-widget.getwarmly.com/warmly.js?clientId=3ff4ae8d4e302fc1a08242a1a3d6b858" defer></script>
+<>
+    <script id="warmly-script-loader" src="https://opps-widget.getwarmly.com/warmly.js?clientId=3ff4ae8d4e302fc1a08242a1a3d6b858" defer></script>
+    <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/49109312.js"></script>
+</>
